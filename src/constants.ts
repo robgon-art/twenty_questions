2485a67eae7edde00f413f7497909de055d44874
Rob Gonsalves <rob.gonsalves@avid.com>
2025-03-29 12:10:32 -0700

    make it 20 questions again

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // The number of questions for the game
-export const MAX_QUESTIONS = 3;
+export const MAX_QUESTIONS = 20;
 
 // Common items for the game (to be avoided)
 export const commonTwentyQuestionsItems: string[] = [
